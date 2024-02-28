aaf957e309b7a21bcd64ab89227fb9a366e837af
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-02-28 21:50:22 +0800

    linear search.py

---
+++ linear search.py
@@ -0,0 +1,21 @@
+Item = int(input("pick a number from 1-10 "))
+Itemlist = [4,1,2,3,8,5,6,9,10,7]
+
+def LinearSearch(n):  
+  Found = False
+  loop = 0
+  for i in range (len(Itemlist)):
+      if (Itemlist[int(i)]) == (int(Item)):
+        print ("found it")
+        print ("item number",i + 1,"in the array")
+        Found = True
+        break
+      else:
+        loop = loop + 1
+        
+  if Found == False:
+    print ("not found")
+  print ("number of items in the list:",len(Itemlist))
+  print ("number of loops: ",loop)
+
+LinearSearch(Item)
