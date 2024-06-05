9bbe3ab4d2a1df505ca128746331f84e5adac7fb
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-06-05 16:27:44 +0800

    caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,18 +1,26 @@
-letter=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-plaintext = input("input the text to be ciphered: ")
-key = int(input("shift number? "))
-def encode(plaintext,key):
-    cipher = ""
-    text = plaintext.lower()
-    for lettersintext in text:
-        if lettersintext ==" ":
-            cipher+=" "
+def caesar_cipher_encode(text, shift):
+    global encrypted_text
+    encrypted_text = ""
+    for char in text:
+        # Encrypt uppercase letters
+        if char.isupper():
+            encrypted_text += chr((ord(char) + shift - 65) % 26 + 65)
+        # Encrypt lowercase letters
+        elif char.islower():
+            encrypted_text += chr((ord(char) + shift - 97) % 26 + 97)
+        # Ignore non-alphabetic characters
         else:
-            for i in range(len(letter)):
-                if letter[i] == lettersintext:
-                    cipher += letter[i+key]
-                    break
-    return cipher
+            encrypted_text += char
+    return encrypted_text
 
-print(encode(plaintext,key))
+def caesar_cipher_decode(text, shift):
+    global decrypted_text
+    decrypted_text = caesar_cipher_encode(text, -shift)
+    return decrypted_text
+
+if __name__ == "__main__":
+    shift = 3
+    caesar_cipher_encode("xavier.", shift)
+    print ("After encryption: "+encrypted_text)
+    caesar_cipher_decode(encrypted_text, shift)
+    print ("After decryption: "+decrypted_text)
