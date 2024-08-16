917c2693c3a63012be9440cce382a65db8ddf37a
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-08-16 17:33:22 +0800

    Update Algorithms Demo.py

--- Algorithms Demo.py
+++ Algorithms Demo.py
@@ -24,15 +24,20 @@ def demo_algorithms():
     time.sleep(1.5)
     print ("~ Next is: 'Selection'. The ability to choose the next action between multiple choices.")
     time.sleep(3)
-    choice = str(input("Would you like the phrase 'Hello World!' to be printed? (1 = yes, 2 = no).\n"))
-    if choice == "1":
-        print ("Hello World!")
-        time.sleep(1)
-        print ("1 was inputted, the phrase was printed.")
-    elif choice == "2":
-        print ("2 was inputted, the phrase was not printed.")
-    else:
-        print ("Input was not recognised. Moving on...")
+    valid = False
+    while valid == False:
+        choice = str(input("Would you like the phrase 'Hello World!' to be printed? (1 = yes, 2 = no).\n"))
+        if choice == "1":
+            print ("Hello World!")
+            valid = True
+            time.sleep(1)
+            print ("1 was inputted, the phrase was printed.")
+        elif choice == "2":
+            print ("2 was inputted, the phrase was not printed.")
+            valid = True
+        else:
+            print ("Input was not recognised. Try again.")
+            
     time.sleep(2)
     print ("Selection can also be condition-based.")
     time.sleep(1)
@@ -40,12 +45,21 @@ def demo_algorithms():
     time.sleep(1.5)
     print ("~ Next is: 'Repetition'/'Iteration'. The action of repeating a step more than once.")
     time.sleep(2)
-    count = int(input(("How many times would you like the phrase 'Hello World!' to be printed? (Numbers only)\n")))
-    for i in range(count):
-        print("Hello World!")
-        time.sleep(1)
+    worked = False
+    while worked == False:
+        try:
+            count = int(input(("How many times would you like the phrase 'Hello World!' to be printed? (Numbers only)\n")))
+            for i in range(count):
+                print("Hello World!")
+                time.sleep(1)
+                worked = True
+        except:
+            print ("Input was not a whole number. Try again.")
     time.sleep(0.5)
-    print (f"'Hello World!' was printed {count} time(s).")
+    if count == 1:
+        print ("Hello world! was printed 1 time.")
+    else:
+        print (f"'Hello World!' was printed {count} time(s).")
     time.sleep(1.5)
     print ("Iteration can also be used conditionally, in which the code will repeat until a condition is met.")
     time.sleep(1)
@@ -55,7 +69,7 @@ def demo_algorithms():
     time.sleep(2)
     variable = input("Whatever you type next will be stored in a variable and printed again.\n")
     time.sleep(1.5)
-    print (variable)
+    print (f"{variable} was stored in the variable.")
     time.sleep(1)
     print ("--------------------------------------------------------------------------------------------")
     time.sleep(2)
