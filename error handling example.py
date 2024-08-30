7541b0931e9d0be2902067e67eab0dd76fd9d7ad
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-08-30 18:29:26 +0800

    error handling example.py

---
+++ error handling example.py
@@ -0,0 +1,9 @@
+    # while worked == False:
+    #     try:
+    #         count = int(input(("How many times would you like the phrase 'Hello World!' to be printed? (Numbers only)\n")))
+    #         for i in range(count):
+    #             print("Hello World!")
+    #             time.sleep(1)
+    #             worked = True
+    #     except:
+    #         print ("Input was not a whole number. Try again.")
