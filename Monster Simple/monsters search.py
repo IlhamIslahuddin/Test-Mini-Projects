7c856c55f783ee6c6987005b99962bad6bbf3b76
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-08-30 19:01:29 +0800

    monsters search.py

---
+++ Monster Simple/monsters search.py
@@ -0,0 +1,17 @@
+def search_monster():
+    searching_for = str(input("What is the name of the monster you are looking for? "))
+    lower = searching_for.lower()
+    file = open("monsters simple.txt", "r")
+    lines = file.readlines()
+    found = False
+    for line in lines:
+        split = line.split(",")
+        if (split[0]).lower() == lower:
+            print (line)
+            found = True
+            break
+    if found == False:
+        print (f"Couldnt find a monster named {searching_for}.")
+
+if __name__ == "__main__":
+    search_monster()
