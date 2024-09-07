a751cae46fecaa78fbd81920662479988d0f37da
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-09-07 12:50:15 +0800

    Create Performance_Counter.py

---
+++ Performance_Counter.py
@@ -0,0 +1,10 @@
+#measuring execution time of code
+import time
+start = time.perf_counter()
+
+
+# YOUR CODE GOES HERE
+
+
+end = time.perf_counter()
+print(f"Elapsed time: {end - start:.1f} seconds")
