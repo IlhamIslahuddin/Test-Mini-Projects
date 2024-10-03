29f2ec414b544b6c9ddc4ce171ede7eb72d3b022
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-10-03 08:47:18 +0800

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -1,28 +1,18 @@
-array_to_sort = [6,4,43,2,22,2,34,4,5]
+array = [6,26,4,43,2,22,2,34,4,5,2,33,5,7,6,5,4567]
 
-def sort():
-    temp = 0
-    num_right = 0
-    x = 0
-    for i in range(99):
-        if num_right >= len(array_to_sort)-1:
-            break
-        x += 1
-        for j in range (len(array_to_sort)):
-            if num_right >= len(array_to_sort)-1:
-                break
-            if j == len(array_to_sort) - 1:
-                pass
-            elif array_to_sort[j] > array_to_sort[j+1]:
+def sort(array_to_sort):
+    noSwaps = False
+    for i in range(len(array_to_sort)):
+        noSwaps = True
+        for j in range(len(array_to_sort)-1):
+            if array_to_sort[j] > array_to_sort[j+1]:
+                noSwaps = False
                 temp = array_to_sort[j]
                 array_to_sort[j] = array_to_sort[j+1]
                 array_to_sort[j+1] = temp
-                num_right = 0
-            else:
-                num_right += 1
-    print ("done after",x,"laps")
-    print (array_to_sort)
+        if noSwaps == True:
+            return array_to_sort
+            
 
 if __name__ == "__main__":
-    sort()
-    pass
+    print (sort(array))
