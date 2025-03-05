5da20a91c5d6123b1717d3154eede6245ecb7047
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2025-03-05 22:48:36 +0800

    Create removing \n from print loops.py

---
+++ removing \n from print loops.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    for i in range (3):
+        print (i)
+    
+    #changing the end of the line from \n to a space
+    for i in range (3):
+        print (i, end=" ")
